26297a6c7731fb11c70d0b894151d423f10f8e7f
Eli Selkin <ejselkin@cpp.edu>
2018-02-17 23:31:54 -0800

    now preventing labeling for non-signed in people

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { connect } from "react-redux";
 import "./App.css";
 import Labeling from "./Components/labeling";
 import Nav from "./Components/nav";
@@ -43,15 +44,36 @@ class App extends Component {
               render={props => <News props={props} />}
             />
             {/* labeling task routes */}
-            <Route
-              exact
-              path="/labeling"
-              render={props => <Labeling props={props} />}
-            />
+            {this.props.labeling}
           </Switch>
         </div>
       </BrowserRouter>
     );
   }
 }
+const mapStateToProps = state => {
+  console.log("APP MAPSTATETOPROPS", state);
+  let labeling = null;
+  if (state.logged_in) {
+    labeling = (
+      <Route
+        exact
+        path="/labeling"
+        render={props => <Labeling props={props} />}
+      />
+    );
+  }
+  return {
+    logged_in: state.logged_in,
+    signed_up: state.signed_up,
+    session: state.session,
+    labeling: labeling
+  };
+};
+const mapDispatchToProps = dispatch => {
+  console.log("APP MAPDISPATCHTOPROPS", dispatch);
+  return {};
+};
+
+App = connect(mapStateToProps, mapDispatchToProps)(App);
 export default App;
